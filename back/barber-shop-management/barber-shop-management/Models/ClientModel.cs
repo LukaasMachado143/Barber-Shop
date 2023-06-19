1943fa27bf4cfb5ba4a6c8d141f9733c39e38b95
Lucas Machado <lucas.fernando@predify.me>
2023-06-19 14:16:01 -0300

    limpeza de arquivos, criação do modelo de cliente e da interface do repository do mesmo

---
+++ back/barber-shop-management/barber-shop-management/Models/ClientModel.cs
@@ -0,0 +1,25 @@
+﻿namespace barber_shop_management.Models
+{
+    public class ClientModel
+    {
+
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string birthDate { get; set; }
+        public string? cpf { get; set; }
+        public string rg { get; set; }
+        public string? address { get; set; }
+        public string phoneNumber { get; set; }
+        public ClientModel(Guid id, string name, string birthDate, string? cpf, string rg, string? address, string phoneNumber)
+        {
+            Id = id;
+            this.name = name;
+            this.birthDate = birthDate;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
+    }
+}
---
+++ back/barber-shop-management/barber-shop-management/Models/ClientRepositoryInterface.cs
@@ -0,0 +1,13 @@
+﻿namespace barber_shop_management.Models
+{
+    public interface IClientRepository
+    {
+        void AddClient(ClientModel client);
+        void UpdateDataClient(ClientModel client);
+        void DeleteClient(ClientModel client);
+        List<ClientModel> GetAllClient();
+
+
+
+    }
+}
